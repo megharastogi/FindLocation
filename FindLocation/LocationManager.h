d9d8ba1ad16d9dda58757fe052949a108b06406f
Megha Gulati <meghagulati30@gmail.com>
2013-09-23 13:30:35 -0700

    first commit

---
+++ FindLocation/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  FindLocation
+//
+//  Created by MEGHA GULATI on 9/23/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ FindLocation/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  FindLocation
+//
+//  Created by MEGHA GULATI on 9/23/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
