5846097de0dcce16f43283fd91d8635ccffcb275
Megha Gulati <meghagulati30@gmail.com>
2013-09-23 12:45:50 -0700

    Initial Commit

---
+++ FindLocation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FindLocation
+//
+//  Created by MEGHA GULATI on 9/23/13.
+//  Copyright (c) 2013 MEGHA GULATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
